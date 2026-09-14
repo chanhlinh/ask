4c1abfb262b33c218bc23b02687360fad2491b40
chanhlinh <64213714+chanhlinh@users.noreply.github.com>
2026-09-15 01:01:53 +0700

    fix: provide a console-safe application URL default

--- config/app.php
+++ config/app.php
@@ -1,2 +1,14 @@
 <?php
-return ['name' => env('APP_NAME', 'Ask'), 'env' => env('APP_ENV', 'production'), 'debug' => (bool) env('APP_DEBUG', false), 'url' => env('APP_URL'), 'timezone' => env('APP_TIMEZONE', 'UTC'), 'locale' => 'en', 'fallback_locale' => 'en', 'key' => env('APP_KEY'), 'cipher' => 'AES-256-CBC', 'maintenance' => ['driver' => 'file']];
+
+return [
+    'name' => env('APP_NAME', 'Ask'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'locale' => 'en',
+    'fallback_locale' => 'en',
+    'key' => env('APP_KEY'),
+    'cipher' => 'AES-256-CBC',
+    'maintenance' => ['driver' => 'file'],
+];
